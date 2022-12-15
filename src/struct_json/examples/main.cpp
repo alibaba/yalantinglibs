96763e0e7d104e50bb427ac63f58773016c32d3b
qicosmos <qicosmos@linux.alibaba.com>
2022-12-15 15:16:56 +0800

    update

--- src/struct_json/examples/main.cpp
+++ src/struct_json/examples/main.cpp
@@ -38,12 +38,78 @@ REFLECTION(json_struct, hash_code, struct_name, fileds);
 std::string id_to_string(struct_pack::detail::type_id id) {
   using namespace struct_pack::detail;
   switch (id) {
+    case type_id::compatible_t:
+      return "compatible";
     case type_id::int32_t:
       return "int32";
     case type_id::uint32_t:
       return "uint32";
+    case type_id::int64_t:
+      return "int64";
+    case type_id::uint64_t:
+      return "uint64";
+    case type_id::int8_t:
+      return "int8";
+    case type_id::uint8_t:
+      return "uint8";
+    case type_id::int16_t:
+      return "int16";
+    case type_id::uint16_t:
+      return "uint16";
+    case type_id::int128_t:
+      return "int128";
+    case type_id::uint128_t:
+      return "uint128";
+    case type_id::bool_t:
+      return "bool";
+    case type_id::char_8_t:
+      return "char_8";
+    case type_id::char_16_t:
+      return "char_16";
+    case type_id::char_32_t:
+      return "char_32";
+    case type_id::w_char_t:
+      return "w_char";
+    case type_id::float16_t:
+      return "float16";
+    case type_id::float32_t:
+      return "float32";
+    case type_id::float64_t:
+      return "float64";
+    case type_id::float128_t:
+      return "float128";
+    case type_id::v_int32_t:
+      return "v_int32";
+    case type_id::v_int64_t:
+      return "v_int64";
+    case type_id::v_uint32_t:
+      return "v_uint32";
+    case type_id::v_uint64_t:
+      return "v_uint64";
     case type_id::string_t:
       return "string";
+    case type_id::array_t:
+      return "array";
+    case type_id::map_container_t:
+      return "map_container";
+    case type_id::set_container_t:
+      return "set_container";
+    case type_id::container_t:
+      return "container";
+    case type_id::optional_t:
+      return "optional";
+    case type_id::variant_t:
+      return "variant";
+    case type_id::expected_t:
+      return "expected";
+    case type_id::monostate_t:
+      return "monostate";
+    case type_id::circle_flag:
+      return "circle_flag";
+    case type_id::trivial_class_t:
+      return "trivial_class";
+    case type_id::non_trivial_class_t:
+      return "non_trivial_class";
     default:
       throw std::invalid_argument("unknown type id");
   }
