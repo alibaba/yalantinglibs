7b3d18da9b2bf4cd05dbe366cf574dd1710ba5ea
helintongh <agh6399@qq.com>
2023-02-07 08:48:56 +0800

    [easylog]add severity,flush etc. unittest case

--- src/easylog/tests/test_easylog.cpp
+++ src/easylog/tests/test_easylog.cpp
@@ -81,3 +81,77 @@ TEST_CASE("test basic") {
   CHECK(get_last_line(other_filename).rfind("it is a test 42") !=
         std::string::npos);
 }
+
+TEST_CASE("test_severity") {
+  // test severity
+  std::string severity_filename = "test_severity.txt";
+  std::filesystem::remove(severity_filename);
+  constexpr size_t SecId = 3;
+  easylog::init_log<SecId>(Severity::WARN, severity_filename, true, 5000, 1,
+                           true);
+
+  MELOGV(ERROR, SecId, "error log can put in file.");
+  MELOGV(INFO, SecId, "info log can't put in file.");
+  CHECK(get_last_line(severity_filename).rfind("info log can't put in file.") ==
+        std::string::npos);
+  CHECK(get_last_line(severity_filename).rfind("error log can put in file.") !=
+        std::string::npos);
+
+  ELOG(INFO, SecId) << "info log can't write to file.";
+  CHECK(
+      get_last_line(severity_filename).rfind("info log can't write to file.") ==
+      std::string::npos);
+  ELOG(WARN, SecId) << "warn log can write to file.";
+  CHECK(get_last_line(severity_filename).rfind("warn log can write to file.") !=
+        std::string::npos);
+}
+
+TEST_CASE("test_flush") {
+  // test flush
+  std::string flust_filename = "test_flush.txt";
+  std::filesystem::remove(flust_filename);
+  constexpr size_t ThirdId = 4;
+  easylog::init_log<ThirdId>(Severity::DEBUG, flust_filename, true, 5000, 1,
+                             false);
+
+  ELOG(INFO, ThirdId) << "flush log to file.";
+  CHECK(get_last_line(flust_filename).rfind("flush log to file.") ==
+        std::string::npos);
+  easylog::flush<ThirdId>();
+  CHECK(get_last_line(flust_filename).rfind("flush log to file.") !=
+        std::string::npos);
+}
+
+bool file_exists(const std::string& path) {
+  std::filesystem::path file(path.c_str());
+  if (std::filesystem::exists(file)) {
+    return true;
+  }
+  return false;
+}
+
+TEST_CASE("file_number") {
+  std::string number_file = "test_number.txt";
+  std::filesystem::remove(number_file);
+  constexpr size_t FourthId = 5;
+  easylog::init_log<FourthId>(Severity::DEBUG, number_file, true, 23, 4, true);
+  MELOGV(INFO, FourthId, "append data to file1.");
+  MELOGV(INFO, FourthId, "append data to file2.");
+  MELOGV(INFO, FourthId, "append data to file3.");
+
+  CHECK(file_exists("test_number.1.txt") == true);
+  CHECK(file_exists("test_number.2.txt") == true);
+}
+
+TEST_CASE("file_roll") {
+  std::string roll_file = "roll_file.txt";
+  std::filesystem::remove(roll_file);
+  constexpr size_t FifthId = 6;
+  easylog::init_log<FifthId>(Severity::DEBUG, roll_file, true, 10, 1, true);
+  MELOGV(INFO, FifthId, "should be covered string.");
+  MELOGV(INFO, FifthId, "the string that should be saved in the file.");
+  CHECK(get_last_line(roll_file).rfind(
+            "he string that should be saved in the file.") !=
+        std::string::npos);
+}
+
