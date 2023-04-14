010dab21f2a7601e0aea31651e15dbf01a9f74ad
sunflower-knight <dglz.hyj@qq.com>
2023-04-14 17:13:14 +0800

    add en and zh article data and theme config

---
+++ website/.vitepress/config/en_data.ts
@@ -0,0 +1,33 @@
+export const guidLinks = [
+    {text: 'Introduce YalantingLibs', link: '/guide/what_is_yalantinglibs'},
+    {text: 'Use as Git Submodule', link: '/guide/how_to_use_as_git_submodule'},
+    {text: 'Use by CMake find_package', link: '/guide/how_to_use_by_cmake_find_package'},
+];
+
+//构建系统相关语法
+export const struct_pb_Links = [
+    {text: 'What is struct_pb?', link: '/struct_pb/struct_pb_intro'},
+    {text: 'Quick Start', link: '/struct_pb/struct_pb_quick_start'},
+    {text: 'Supported Features', link: '/struct_pb/struct_pb_supported_features'},
+    {text: 'Guide (proto3)', link: '/struct_pb/struct_pb_guide_proto3'},
+    {text: 'Generating your struct', link: '/struct_pb/struct_pb_generating_your_struct'},
+    {text: 'struct_pb API', link: '/struct_pb/struct_pb_api'},
+];
+
+export const struct_pack_Links = [
+    {text: 'What is struct_pack?', link: '/struct_pack/struct_pack_intro'},
+    {text: 'struct_pack layout', link: '/struct_pack/struct_pack_layout'},
+    {text: 'struct_pack type system', link: '/struct_pack/struct_pack_type_system'},
+    {text: 'API Reference', link: "https://alibaba.github.io/yalantinglibs/en/html/group__struct__pack.html"}
+];
+
+export const coro_rpc_Links = [
+    {text: 'What is coro_rpc?', link: '/coro_rpc/coro_rpc_introduction'},
+];
+
+export const aboutLinks = [
+    {text: 'purecpp', link: '/about/community'},
+    {text: 'contribute', link: '/about/contribute'},
+    {text: 'community', link: '/about/teams'},
+    {text: 'team', link: '/about/yalantingUser'},
+];
---
+++ website/.vitepress/config/zh_data.ts
@@ -0,0 +1,33 @@
+export const guidLinks = [
+    {text: 'Introduce YalantingLibs', link: '/zh/guide/what_is_yalantinglibs'},
+    {text: 'Use as Git Submodule', link: '/zh/guide/how_to_use_as_git_submodule'},
+    {text: 'Use by CMake find_package', link: '/zh/guide/how_to_use_by_cmake_find_package'},
+];
+
+//构建系统相关语法
+export const struct_pb_Links = [
+    {text: 'What is struct_pb?', link: '/zh/struct_pb/struct_pb_intro'},
+    {text: 'Quick Start', link: '/zh/struct_pb/struct_pb_quick_start'},
+    {text: 'Supported Features', link: '/zh/struct_pb/struct_pb_supported_features'},
+    {text: 'Guide (proto3)', link: '/zh/struct_pb/struct_pb_guide_proto3'},
+    {text: 'Generating your struct', link: '/zh/struct_pb/struct_pb_generating_your_struct'},
+    {text: 'struct_pb API', link: '/zh/struct_pb/struct_pb_api'},
+];
+
+export const struct_pack_Links = [
+    {text: 'What is struct_pack?', link: '/zh/struct_pack/struct_pack_intro'},
+    {text: 'struct_pack layout', link: '/zh/struct_pack/struct_pack_layout'},
+    {text: 'struct_pack type system', link: '/zh/struct_pack/struct_pack_type_system'},
+    {text: 'API Reference', link: "https://alibaba.github.io/yalantinglibs/en/html/group__struct__pack.html"}
+];
+
+export const coro_rpc_Links = [
+    {text: 'What is coro_rpc?', link: '/zh/coro_rpc/coro_rpc_introduction'},
+];
+
+export const aboutLinks = [
+    {text: 'purecpp', link: '/zh/about/community'}, //TODO 未来支持英语
+    {text: 'contribute', link: '/zh/about/contribute'},
+    {text: 'community', link: '/zh/about/teams'},
+    {text: 'team', link: '/zh/about/yalantingUser'},
+];
