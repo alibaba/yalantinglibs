a9b5a8fd3a478cb8c85c0669af3fe78be3fcfe76
sunflower-knight <dglz.hyj@qq.com>
2023-04-14 17:11:25 +0800

    change index.ts ,make it easy to read and prepare locale config

--- website/.vitepress/config/index.ts
+++ website/.vitepress/config/index.ts
@@ -1,19 +1,35 @@
-export default {
-  base: '/yalantinglibs/',
-  lastUpdated: true,
-  ignoreDeadLinks: false,
-  outDir: "dist",
+import {defineConfig} from "vitepress";
 
-  head: [],
+import {en_LocaleConfig} from "./en_locale";
+import {zh_LocaleConfig} from "./zh_locale";
 
-  themeConfig: {
-    socialLinks: [
-      {icon: 'github', link: 'https://github.com/alibaba/yalantinglibs'}
-    ],
+export default defineConfig(
+    {
+        //Resource
+        srcDir: "docs",
+        outDir: "dist",
+        //Website
+        base: '/yalantinglibs/',
+        //Article
+        lastUpdated: true,
+        ignoreDeadLinks: false,
 
-    footer: {
-      message: 'This website is released under the MIT License.',
-      copyright: 'Copyright © 2022 yalantinglibs contributors'
-    },
-  }
-}
+        head: [],
+        //Common theme config
+        themeConfig: {
+            logo: '/icon/logo.svg',
+            socialLinks: [
+                {icon: 'github', link: 'https://github.com/alibaba/yalantinglibs'}
+            ],
+            footer: {
+                message: 'This website is released under the MIT License.',
+                copyright: 'Copyright © 2022 yalantinglibs contributors'
+            },
+        },
+        //Locales config and theme-specify
+        locales: {
+            'root': <any>en_LocaleConfig,
+            'zh': <any>zh_LocaleConfig,
+        },
+    }
+)
