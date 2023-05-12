a25474a63acd4e317bf7f1de0a8b662a7f80f717
qicosmos <qicosmos@linux.alibaba.com>
2023-05-12 11:55:14 +0800

    update for gcc11 (#283)

--- thirdparty/iguana/iguana/detail/string_stream.hpp
+++ thirdparty/iguana/iguana/detail/string_stream.hpp
@@ -5,10 +5,12 @@
 #endif
 
 namespace iguana {
+#ifdef IGUANA_ENABLE_PMR
 #if __has_include(<memory_resource>)
 inline char iguana_buf[2048];
 inline std::pmr::monotonic_buffer_resource iguana_resource(iguana_buf, 2048);
 using string_stream = std::pmr::string;
+#endif
 #else
 using string_stream = std::string;
 #endif
