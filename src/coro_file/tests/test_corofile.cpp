0e951fa5edb9011b6509e0dcec3586a3f50417b2
zhengjian526 <gfkd_zheng@163.com>
2023-05-31 00:12:12 +0800

    add coro_file  write test

--- src/coro_file/tests/test_corofile.cpp
+++ src/coro_file/tests/test_corofile.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <filesystem>
 #include <fstream>
@@ -15,31 +16,32 @@
 constexpr uint64_t KB = 1024;
 constexpr uint64_t MB = 1024 * KB;
 
-std::string create_big_file(std::string filename, size_t file_size, std::string fill_with) {
+std::vector<char> create_big_file(std::string filename, size_t file_size,
+                                  std::string fill_with) {
   std::ofstream file(filename, std::ios::binary);
   file.exceptions(std::ios_base::failbit | std::ios_base::badbit);
 
   if (!file) {
     std::cout << "create file failed\n";
-    return "";
+    return std::vector<char>{};
   }
   size_t fill_with_size = fill_with.size();
   if (file_size == 0 || fill_with_size == 0) {
-    return "";
+    return std::vector<char>{};
   }
-  std::string str;
+  std::vector<char> ret(file_size);
   int cnt = file_size / fill_with_size;
   int remain = file_size % fill_with_size;
   for (int i = 0; i < cnt; i++) {
     file.write(fill_with.data(), fill_with_size);
-    str += fill_with;
+    memcpy(ret.data() + i * fill_with_size, fill_with.data(), fill_with_size);
   }
   if (remain > 0) {
     file.write(fill_with.data(), remain);
-    str += fill_with.substr(remain);
+    memcpy(ret.data() + file_size - remain, fill_with.data(), remain);
   }
   file.flush();  // can throw
-  return str;
+  return ret;
 }
 
 void create_small_file(std::string filename, std::string file_content) {
@@ -60,9 +62,7 @@ void create_small_file(std::string filename, std::string file_content) {
 
 #ifdef ENABLE_FILE_IO_URING
 
-TEST_CASE("test coro http bearer token auth request") {
-
-}
+TEST_CASE("test coro http bearer token auth request") {}
 
 #else
 
@@ -77,16 +77,13 @@ TEST_CASE("small_file_read_test") {
   });
 
   ylt::coro_file file(ioc.get_executor(), filename);
-  bool r = file.is_open();
-  if (!file.is_open()) {
-    return;
-  }
+  CHECK(file.is_open());
 
   char buf[512]{};
   auto [ec, read_size] =
-        async_simple::coro::syncAwait(file.async_read(buf, 512));
+      async_simple::coro::syncAwait(file.async_read(buf, 512));
   if (ec) {
-      std::cout << ec.message() << "\n";
+    std::cout << ec.message() << "\n";
   }
   auto read_content = std::string_view(buf, read_size);
   std::cout << read_size << "\n";
@@ -95,7 +92,34 @@ TEST_CASE("small_file_read_test") {
   CHECK(read_content == file_content);
   work.reset();
   thd.join();
+}
+TEST_CASE("big_file_read_test") {
+  std::string filename = "big_file_read_test.txt";
+  std::string file_with = "abc";
+  uint64_t file_size = 100 * MB;
+  std::vector<char> file_content{
+      create_big_file(filename, file_size, file_with)};
+  asio::io_context ioc;
+  auto work = std::make_unique<asio::io_context::work>(ioc);
+  std::thread thd([&ioc] {
+    ioc.run();
+  });
 
+  ylt::coro_file file(ioc.get_executor(), filename);
+  CHECK(file.is_open());
+
+  std::vector<char> read_content(file_size);
+  auto [ec, read_size] = async_simple::coro::syncAwait(
+      file.async_read(read_content.data(), file_size));
+  if (ec) {
+    std::cout << ec.message() << "\n";
+  }
+  std::cout << read_size << "\n";
+  //   std::cout << read_content << "\n";
+  CHECK(true == std::equal(file_content.begin(), file_content.end(),
+                           read_content.begin()));
+  work.reset();
+  thd.join();
 }
 TEST_CASE("empty_file_read_test") {
   std::string filename = "empty_file_read_test.txt";
@@ -108,16 +132,13 @@ TEST_CASE("empty_file_read_test") {
   });
 
   ylt::coro_file file(ioc.get_executor(), filename);
-  bool r = file.is_open();
-  if (!file.is_open()) {
-    return;
-  }
+  CHECK(file.is_open());
 
   char buf[512]{};
   auto [ec, read_size] =
-        async_simple::coro::syncAwait(file.async_read(buf, 512));
+      async_simple::coro::syncAwait(file.async_read(buf, 512));
   if (ec) {
-      std::cout << ec.message() << "\n";
+    std::cout << ec.message() << "\n";
   }
   auto read_content = std::string_view(buf, read_size);
   std::cout << read_size << "\n";
@@ -126,13 +147,9 @@ TEST_CASE("empty_file_read_test") {
   CHECK(read_content == file_content);
   work.reset();
   thd.join();
-
 }
-TEST_CASE("big_file_read_test") {
-  std::string filename = "big_file_read_test.txt";
-  std::string file_with = "abc";
-  uint64_t file_size = 100 * MB;
-  auto file_content = std::string_view(std::move(create_big_file(filename, file_size, file_with)));
+TEST_CASE("small_file_write_test") {
+  std::string filename = "small_file_write_test.txt";
   asio::io_context ioc;
   auto work = std::make_unique<asio::io_context::work>(ioc);
   std::thread thd([&ioc] {
@@ -140,24 +157,50 @@ TEST_CASE("big_file_read_test") {
   });
 
   ylt::coro_file file(ioc.get_executor(), filename);
-  bool r = file.is_open();
-  if (!file.is_open()) {
+  CHECK(file.is_open());
+
+  char buf[512]{};
+
+  std::string file_content_0 = "small_file_write_test_0";
+
+  auto ec = async_simple::coro::syncAwait(
+      file.async_write(file_content_0.data(), file_content_0.size()));
+  if (ec) {
+    std::cout << ec.message() << "\n";
+  }
+
+  std::ifstream is(filename, std::ios::binary);
+  if (!is.is_open()) {
+    std::cout << "Failed to open file: " << filename << "\n";
     return;
   }
+  is.read(buf, 512);
+  is.close();
+  auto read_content = std::string_view(buf, file_content_0.size());
+  std::cout << read_content << "\n";
+  CHECK(read_content == file_content_0);
 
-  std::vector<char> buf(file_size);
-  auto [ec, read_size] =
-        async_simple::coro::syncAwait(file.async_read(buf.data(), file_size));
+  std::string file_content_1 = "small_file_write_test_1";
+
+  ec = async_simple::coro::syncAwait(
+      file.async_write(file_content_1.data(), file_content_1.size()));
   if (ec) {
-      std::cout << ec.message() << "\n";
+    std::cout << ec.message() << "\n";
   }
-  auto read_content = std::string_view(buf.data(), read_size);
-  std::cout << read_size << "\n";
-//   std::cout << read_content << "\n";
-  CHECK(read_size == file_size);
-  CHECK(read_content == file_content);
+  is.open(filename, std::ios::binary);
+  if (!is.is_open()) {
+    std::cout << "Failed to open file: " << filename << "\n";
+    return;
+  }
+  is.read(buf, 512);
+  is.close();
+  read_content =
+      std::string_view(buf, file_content_0.size() + file_content_1.size());
+  std::cout << read_content << "\n";
+  CHECK(read_content == (file_content_0 + file_content_1));
+
   work.reset();
   thd.join();
-
 }
+
 #endif
