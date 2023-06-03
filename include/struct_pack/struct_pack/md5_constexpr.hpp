00f0dc25a2de8be326e81a3bfc2ad57cc5843894
sunflower-knight <dglz.hyj@qq.com>
2023-06-03 19:19:55 +0800

    [fix] fix format

--- include/struct_pack/struct_pack/md5_constexpr.hpp
+++ include/struct_pack/struct_pack/md5_constexpr.hpp
@@ -88,8 +88,8 @@ string_literal(const CharType (&value)[Size])
 
 template <typename CharType, size_t Len1, size_t Len2>
 consteval auto operator+(string_literal<CharType, Len1> str1,
-                                   string_literal<CharType, Len2> str2) {
-  string_literal<CharType, Len1 + Len2> ret {};
+                         string_literal<CharType, Len2> str2) {
+  string_literal<CharType, Len1 + Len2> ret{};
   // don't use std::copy_n here to support low version stdlibc++
   for (size_t i = 0; i < Len1; ++i) {
     ret[i] = str1[i];
