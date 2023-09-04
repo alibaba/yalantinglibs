3f39a9ae9dc94ae0fc61871be5553de8615b64f3
qicosmos <qicosmos@linux.alibaba.com>
2023-09-04 14:36:47 +0800

    compile

--- include/ylt/easylog.hpp
+++ include/ylt/easylog.hpp
@@ -243,7 +243,7 @@ inline void add_appender(std::function<void(std::string_view)> fn) {
                           GET_STRING(__FILE__, __LINE__))             \
             .format(prefix::format(format_str, __VA_ARGS__));         \
     if (severity == easylog::Severity::CRITICAL ||                    \
-        record.get_severity() == Severity::FATAL) {                   \
+        severity == Severity::FATAL) {                                \
       easylog::flush<Id>();                                           \
       std::exit(EXIT_FAILURE);                                        \
     }                                                                 \
