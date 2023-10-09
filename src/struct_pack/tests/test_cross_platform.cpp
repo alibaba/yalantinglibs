7ca06684796fa3c5a3961ef872379d7f4ae17916
Zezheng.Li <Zezheng.Li@linux.alibaba.com>
2023-10-09 14:56:06 +0800

    [struct_pack] add test for cross_platform

---
+++ src/struct_pack/tests/test_cross_platform.cpp
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <fstream>
+#include <ylt/struct_pack.hpp>
+
+#include "doctest.h"
+#include "test_struct.hpp"
+using namespace struct_pack;
+using namespace doctest;
+
+TEST_CASE("testing deserialize other platform data") {
+  std::ifstream ifs("binary_data/test_cross_platform.dat");
+  REQUIRE(ifs.is_open());
+  auto result = struct_pack::deserialize<complicated_object>(ifs);
+  REQUIRE(result.has_value());
+  auto object = create_complicated_object();
+  CHECK(result.value() == object);
+}
+
+TEST_CASE("testing deserialize other platform data without debug info") {
+  std::ifstream ifs("binary_data/test_cross_platform_without_debug_info.dat");
+  REQUIRE(ifs.is_open());
+  auto result = struct_pack::deserialize<complicated_object>(ifs);
+  REQUIRE(result.has_value());
+  auto object = create_complicated_object();
+  CHECK(result.value() == object);
+}
+
+TEST_CASE("testing serialize other platform data") {
+  std::ifstream ifs("binary_data/test_cross_platform.dat");
+  REQUIRE(ifs.is_open());
+  std::string content(std::istreambuf_iterator<char>{ifs},
+                      std::istreambuf_iterator<char>{});
+  auto object = create_complicated_object();
+  auto buffer =
+      struct_pack::serialize<std::string,
+                             struct_pack::type_info_config::enable>(object);
+  CHECK(buffer == content);
+}
+
+TEST_CASE("testing serialize other platform data") {
+  std::ifstream ifs("binary_data/test_cross_platform_without_debug_info.dat");
+  REQUIRE(ifs.is_open());
+  std::string content(std::istreambuf_iterator<char>{ifs},
+                      std::istreambuf_iterator<char>{});
+  auto object = create_complicated_object();
+  auto buffer =
+      struct_pack::serialize<std::string,
+                             struct_pack::type_info_config::disable>(object);
+  CHECK(buffer == content);
+}
