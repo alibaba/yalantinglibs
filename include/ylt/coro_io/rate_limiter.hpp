544a6a4a93dd012c6c6c5f51ce758f796d10da0c
时起 <shuaibing.zsb@alibaba-inc.com>
2023-10-30 17:59:44 +0800

    format code using clang-format

--- include/ylt/coro_io/rate_limiter.hpp
+++ include/ylt/coro_io/rate_limiter.hpp
@@ -15,13 +15,14 @@
  */
 #pragma once
 
-#include <iostream>
 #include <async_simple/coro/Lazy.h>
-#include <async_simple/coro/Sleep.h>
 #include <async_simple/coro/Mutex.h>
+#include <async_simple/coro/Sleep.h>
 #include <async_simple/coro/SyncAwait.h>
+
 #include <algorithm>
 #include <chrono>
+#include <iostream>
 #include <ylt/easylog.hpp>
 
 namespace coro_io {
@@ -39,13 +40,16 @@ class RateLimiter {
     doSetRate(permitsPerSecond, currentTimeMills());
     this->mutex_.unlock();
   }
-  
+
  protected:
   virtual void doSetRate(double permitsPerSecond, long now_micros) = 0;
   virtual long reserveEarliestAvailable(int permits, long now_micros) = 0;
   long currentTimeMills() {
-    std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
+    std::chrono::system_clock::time_point now =
+        std::chrono::system_clock::now();
+    std::chrono::milliseconds ms =
+        std::chrono::duration_cast<std::chrono::milliseconds>(
+            now.time_since_epoch());
     return ms.count();
   }
 
@@ -60,15 +64,20 @@ class RateLimiter {
 
 class AbstractSmoothRateLimiter : public RateLimiter {
  protected:
-  virtual void doSetRate(double permits_per_second, double stable_internal_micros) = 0;
-  virtual long storedPermitsToWaitTime(double stored_permits, double permits_to_take) = 0;
+  virtual void doSetRate(double permits_per_second,
+                         double stable_internal_micros) = 0;
+  virtual long storedPermitsToWaitTime(double stored_permits,
+                                       double permits_to_take) = 0;
   virtual double coolDownInternalMicros() = 0;
   void resync(long now_micros) {
     // if next_free_ticket is in the past, resync to now
-    ELOG_DEBUG << "now micros: " << now_micros << ", next_free_ticket_micros_: " << this->next_free_ticket_micros_;
+    ELOG_DEBUG << "now micros: " << now_micros << ", next_free_ticket_micros_: "
+               << this->next_free_ticket_micros_;
     if (now_micros > this->next_free_ticket_micros_) {
-      double newPermits = (now_micros - this->next_free_ticket_micros_) / coolDownInternalMicros();
-      this->stored_permits_ = std::min(this->max_permits_, this->stored_permits_ + newPermits);
+      double newPermits = (now_micros - this->next_free_ticket_micros_) /
+                          coolDownInternalMicros();
+      this->stored_permits_ =
+          std::min(this->max_permits_, this->stored_permits_ + newPermits);
       this->next_free_ticket_micros_ = now_micros;
     }
   }
@@ -81,10 +90,12 @@ class AbstractSmoothRateLimiter : public RateLimiter {
   long reserveEarliestAvailable(int required_permits, long now_micros) {
     resync(now_micros);
     long return_value = this->next_free_ticket_micros_;
-    double stored_permits_to_spend = std::min((double) required_permits, this->stored_permits_);
+    double stored_permits_to_spend =
+        std::min((double)required_permits, this->stored_permits_);
     double fresh_permits = required_permits - stored_permits_to_spend;
-    long wait_micros = storedPermitsToWaitTime(this->stored_permits_, stored_permits_to_spend) 
-      + (long) (fresh_permits * this->stable_internal_micros_);
+    long wait_micros = storedPermitsToWaitTime(this->stored_permits_,
+                                               stored_permits_to_spend) +
+                       (long)(fresh_permits * this->stable_internal_micros_);
     this->next_free_ticket_micros_ += wait_micros;
     this->stored_permits_ -= stored_permits_to_spend;
     return return_value;
@@ -99,13 +110,14 @@ class AbstractSmoothRateLimiter : public RateLimiter {
    */
   double max_permits_ = 0;
   /**
-   * The interval between two unit requests, at our stable rate. E.g., a stable rate of 5 permits
-   * per second has a stable internal of 200ms.
+   * The interval between two unit requests, at our stable rate. E.g., a stable
+   * rate of 5 permits per second has a stable internal of 200ms.
    */
   double stable_internal_micros_ = 0;
   /**
-   * The time when the next request (no matter its size) will be granted. After granting a request,
-   * this is pushed further in the future. Large requests push this further than small requests.
+   * The time when the next request (no matter its size) will be granted. After
+   * granting a request, this is pushed further in the future. Large requests
+   * push this further than small requests.
    */
   long next_free_ticket_micros_ = 0;
 };
@@ -116,28 +128,30 @@ class SmoothBurstyRateLimiter : public AbstractSmoothRateLimiter {
     this->max_burst_seconds_ = 1.0;
     async_simple::coro::syncAwait(setRate(permits_per_second));
   }
- 
+
  protected:
   void doSetRate(double permits_per_second, double stable_internal_micros) {
     double old_max_permits = this->max_permits_;
     this->max_permits_ = this->max_burst_seconds_ * permits_per_second;
-    this->stored_permits_ = (0 == old_max_permits) ? 0 
-      : this->stored_permits_ * this->max_permits_ / old_max_permits;
-    ELOG_DEBUG << "max_permits_: " << this->max_permits_ << ", stored_permits_:" << this->stored_permits_;
+    this->stored_permits_ =
+        (0 == old_max_permits)
+            ? 0
+            : this->stored_permits_ * this->max_permits_ / old_max_permits;
+    ELOG_DEBUG << "max_permits_: " << this->max_permits_
+               << ", stored_permits_:" << this->stored_permits_;
   }
 
   long storedPermitsToWaitTime(double stored_permits, double permits_to_take) {
     return 0L;
   }
 
-  double coolDownInternalMicros() {
-    return this->stable_internal_micros_;
-  }
+  double coolDownInternalMicros() { return this->stable_internal_micros_; }
 
  private:
-  /** 
-   * The work(permits) of how many seconds can be saved up if the RateLimiter is unused.
+  /**
+   * The work(permits) of how many seconds can be saved up if the RateLimiter is
+   * unused.
    */
   double max_burst_seconds_ = 0;
 };
-}
+}  // namespace coro_io%
--- src/coro_io/tests/test_rate_limiter.cpp
+++ src/coro_io/tests/test_rate_limiter.cpp
@@ -1,11 +1,14 @@
-#include <doctest.h>
-#include <ylt/coro_io/rate_limiter.hpp>
 #include <async_simple/coro/Collect.h>
+#include <doctest.h>
+
 #include <ylt/coro_io/io_context_pool.hpp>
+#include <ylt/coro_io/rate_limiter.hpp>
 
 long currentTimeMills() {
   std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
-  std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
+  std::chrono::milliseconds ms =
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+          now.time_since_epoch());
   return ms.count();
 }
 
@@ -38,10 +41,10 @@ TEST_CASE("test SmoothBurstyRateLimiter single thread") {
   int permits_to_acquire = 5;
   double expected_cost = (permits_to_acquire - 1) * (1 / permits_per_second);
   double cost_diff = 0.1;
-  
+
   long start_mills = currentTimeMills();
   coro_io::SmoothBurstyRateLimiter rateLimiter(permits_per_second);
-  for (int i = 0; i < permits_to_acquire; i ++) {
+  for (int i = 0; i < permits_to_acquire; i++) {
     double waitMills = async_simple::coro::syncAwait(rateLimiter.acquire(1));
     ELOG_INFO << "wait for " << waitMills;
   }
@@ -55,19 +58,21 @@ TEST_CASE("test SmoothBurstyRateLimiter multi coroutine") {
   double permits_per_second = 100.0;
   int num_of_coroutine = 5;
   int permits_to_acquire_every_coroutine = 5;
-  double expected_cost = (num_of_coroutine * permits_to_acquire_every_coroutine - 1) * (1 / permits_per_second);
+  double expected_cost =
+      (num_of_coroutine * permits_to_acquire_every_coroutine - 1) *
+      (1 / permits_per_second);
   double cost_diff = 0.1;
 
   coro_io::SmoothBurstyRateLimiter rateLimiter(permits_per_second);
   auto consumer = [&]() -> async_simple::coro::Lazy<void> {
-    for (int i = 0; i < permits_to_acquire_every_coroutine; i ++) {
+    for (int i = 0; i < permits_to_acquire_every_coroutine; i++) {
       co_await rateLimiter.acquire(1);
     }
   };
 
   auto consumerListLazy = [&]() -> async_simple::coro::Lazy<void> {
     std::vector<async_simple::coro::Lazy<void>> lazyList;
-    for (int i = 0; i < num_of_coroutine; i ++) {
+    for (int i = 0; i < num_of_coroutine; i++) {
       lazyList.push_back(consumer());
     }
     co_await collectAllPara(std::move(lazyList));
@@ -75,7 +80,8 @@ TEST_CASE("test SmoothBurstyRateLimiter multi coroutine") {
 
   long start_mills = currentTimeMills();
   syncAwait(consumerListLazy().via(
-    coro_io::g_block_io_context_pool<coro_io::multithread_context_pool>(1).get_executor()));
+      coro_io::g_block_io_context_pool<coro_io::multithread_context_pool>(1)
+          .get_executor()));
   double cost = (currentTimeMills() - start_mills) / 1000.0;
 
   CHECK(cost > expected_cost - cost_diff);
