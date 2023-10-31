008254574d12a9393dbc0c694b2e7e2808bd712c
qicosmos <qicosmos@linux.alibaba.com>
2023-10-31 14:32:18 +0800

    revert some

--- cmake/build.cmake
+++ cmake/build.cmake
@@ -23,10 +23,11 @@ if(NOT CMAKE_BUILD_TYPE)
     set(CMAKE_BUILD_TYPE "Release")
 endif()
 message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
-
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+# libc++ or libstdc++&clang
+option(BUILD_WITH_LIBCXX "Build with libc++" OFF)
+message(STATUS "BUILD_WITH_LIBCXX: ${BUILD_WITH_LIBCXX}")
+if(BUILD_WITH_LIBCXX AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-    message(STATUS "Build with libc++")
 else()
 endif()
 
