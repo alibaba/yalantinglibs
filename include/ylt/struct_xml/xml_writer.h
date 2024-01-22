99534576a95a7903898f2f214d4daeb93cfe0aaa
qicosmos <qicosmos@linux.alibaba.com>
2024-01-22 14:35:33 +0800

    using namespace alias to simplify importing iguana

--- include/ylt/struct_xml/xml_writer.h
+++ include/ylt/struct_xml/xml_writer.h
@@ -16,10 +16,4 @@
 #pragma once
 #include <iguana/xml_writer.hpp>
 
-namespace struct_xml {
-template <bool pretty = false, typename Stream, typename T>
-inline void to_xml(T &&t, Stream &s) {
-  iguana::to_xml<pretty>(std::forward<T>(t), s);
-}
-
-}  // namespace struct_xml
+namespace struct_xml = iguana;
--- include/ylt/struct_yaml/yaml_writer.h
+++ include/ylt/struct_yaml/yaml_writer.h
@@ -16,9 +16,4 @@
 #pragma once
 #include <iguana/yaml_writer.hpp>
 
-namespace struct_yaml {
-template <typename Stream, typename T>
-inline void to_yaml(T &&t, Stream &s, size_t min_spaces = 0) {
-  iguana::to_yaml(std::forward<T>(t), s, min_spaces);
-}
-}  // namespace struct_yaml
+namespace struct_yaml = iguana;
