d768e13687208b480970c9be89ebbca193a5b947
qicosmos <qicosmos@linux.alibaba.com>
2024-01-30 12:04:34 +0800

    [coro_http_server][feat]support gzip (#584)

--- coverage_gen.sh
+++ coverage_gen.sh
@@ -25,18 +25,18 @@ mkdir build && cd build || exit 1
 export CC=clang
 export CXX=clang++
 cmake .. -DCOVERAGE_TEST=ON -DENABLE_SSL=ON
-make -j test_rpc
+make -j
 
-# warning: test_rpc.profraw: malformed instrumentation profile data
+# warning: test_ylt.profraw: malformed instrumentation profile data
 # error: no profile can be merged
 # add %m to fix the bug on ARM
 # https://groups.google.com/g/llvm-dev/c/oaA58fbNMGg
 # https://github.com/llvm/llvm-project/issues/50966
-export LLVM_PROFILE_FILE="coro_rpc_test-%m.profraw"
+export LLVM_PROFILE_FILE="ylt_test-%m.profraw"
 ls
-cd tests
-./test_rpc
-llvm-profdata merge -sparse coro_rpc_test-*.profraw -o coro_rpc_test.profdata
-llvm-cov show ./coro_rpc_test -instr-profile=test_rpc.profdata -format=html -output-dir=../../.coverage_llvm_cov -ignore-filename-regex="async_simple|thirdparty|tests|asio|util|logging|struct_pack" -show-instantiations=false
+cd output
+./tests/coro_io_test ./tests/coro_rpc_test ./tests/easylog_test ./tests/struct_pack_test ./tests/struct_pack_test_with_optimize
+llvm-profdata merge -sparse ylt_test-*.profraw -o ylt_test.profdata
+llvm-cov show ./tests/coro_io_test ./tests/coro_rpc_test ./tests/easylog_test ./tests/struct_pack_test ./tests/struct_pack_test_with_optimize -instr-profile=test_ylt.profdata -format=html -output-dir=../../.coverage_llvm_cov -ignore-filename-regex="thirdparty|asio" -show-instantiations=false
 echo 'Done!!!'
 fi
