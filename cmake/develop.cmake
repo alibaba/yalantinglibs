393586c709bdeec2ab32b936052ed7798896d6c2
qicosmos <qicosmos@linux.alibaba.com>
2024-03-15 17:47:56 +0800

    test3

--- cmake/develop.cmake
+++ cmake/develop.cmake
@@ -35,7 +35,7 @@ option(CORO_RPC_USE_OTHER_RPC "coro_rpc extend to support other rpc" OFF)
 message(STATUS "CORO_RPC_USE_OTHER_RPC: ${CORO_RPC_USE_OTHER_RPC}")
 
 # Enable address sanitizer
-option(ENABLE_SANITIZER "Enable sanitizer(Debug+Gcc/Clang/AppleClang)" ON)
+option(ENABLE_SANITIZER "Enable sanitizer(Debug+Clang/AppleClang)" ON)
 if(ENABLE_SANITIZER AND NOT MSVC)
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         check_asan(HAS_ASAN)
