f41f1fb1080a6864e7085c83e76080bb7fb4a7c6
qicosmos <qicosmos@linux.alibaba.com>
2024-03-16 11:05:42 +0800

    select channel and lazy

--- src/coro_io/tests/test_coro_channel.cpp
+++ src/coro_io/tests/test_coro_channel.cpp
@@ -23,6 +23,7 @@ async_simple::coro::Lazy<void> test_coro_channel() {
 
 async_simple::coro::Lazy<void> test_select_channel() {
   using namespace coro_io;
+  using namespace async_simple::coro;
   auto ch1 = coro_io::create_channel<int>(1000);
   auto ch2 = coro_io::create_channel<int>(1000);
 
@@ -31,13 +32,30 @@ async_simple::coro::Lazy<void> test_select_channel() {
 
   std::array<int, 2> arr{41, 42};
   int val;
-  size_t index = co_await select_t(ch1, ch2).on_recieve(
-      [&](std::pair<std::error_code, int> result) {
-        val = result.second;
-      },
-      [&](std::pair<std::error_code, int> result) {
-        val = result.second;
-      });
+
+  size_t index =
+      co_await select(std::pair{async_receive(ch1),
+                                [&val](auto value) {
+                                  auto [ec, r] = value.value();
+                                  val = r;
+                                }},
+                      std::pair{async_receive(ch2), [&val](auto value) {
+                                  auto [ec, r] = value.value();
+                                  val = r;
+                                }});
+
+  CHECK(val == arr[index]);
+
+  co_await async_send(ch1, 41);
+  co_await async_send(ch2, 42);
+
+  std::vector<Lazy<std::pair<std::error_code, int>>> vec;
+  vec.push_back(async_receive(ch1));
+  vec.push_back(async_receive(ch2));
+
+  index = co_await select(std::move(vec), [&](size_t i, auto result) {
+    val = result.value().second;
+  });
   CHECK(val == arr[index]);
 
   period_timer timer1(coro_io::get_global_executor());
@@ -46,32 +64,30 @@ async_simple::coro::Lazy<void> test_select_channel() {
   timer2.expires_after(200ms);
 
   int val1;
-  index = co_await select_t(timer1.async_await(), timer2.async_await())
-              .on_recieve(
-                  [&](bool timeout) {
-                    CHECK(timeout);
-                    val1 = 0;
-                  },
-                  [&](bool timeout) {
-                    CHECK(timeout);
-                    val1 = 1;
-                  });
+  index = co_await select(std::pair{timer1.async_await(),
+                                    [&](auto val) {
+                                      CHECK(val.value());
+                                      val1 = 0;
+                                    }},
+                          std::pair{timer2.async_await(), [&](auto val) {
+                                      CHECK(val.value());
+                                      val1 = 0;
+                                    }});
   CHECK(index == val1);
 
   int val2;
-  index = co_await select_t(coro_io::post([] {
-                            }),
-                            coro_io::post([] {
-                            }))
-              .on_recieve(
-                  [&]() {
-                    std::cout << "post1\n";
-                    val2 = 0;
-                  },
-                  [&]() {
-                    std::cout << "post2\n";
-                    val2 = 1;
-                  });
+  index = co_await select(std::pair{coro_io::post([] {
+                                    }),
+                                    [&](auto) {
+                                      std::cout << "post1\n";
+                                      val2 = 0;
+                                    }},
+                          std::pair{coro_io::post([] {
+                                    }),
+                                    [&](auto) {
+                                      std::cout << "post2\n";
+                                      val2 = 1;
+                                    }});
   CHECK(index == val2);
 
   co_await async_send(ch1, 43);
@@ -79,13 +95,13 @@ async_simple::coro::Lazy<void> test_select_channel() {
   });
 
   int val3 = -1;
-  index = co_await select_t(ch1, lazy).on_recieve(
-      [&](std::pair<std::error_code, int> result) {
-        val3 = result.second;
-      },
-      [&] {
-        val3 = 0;
-      });
+  index = co_await select(std::pair{async_receive(ch1),
+                                    [&](auto result) {
+                                      val3 = result.value().second;
+                                    }},
+                          std::pair{std::move(lazy), [&](auto) {
+                                      val3 = 0;
+                                    }});
 
   if (index == 0) {
     CHECK(val3 == 43);
