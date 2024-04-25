9c2588b0267185c81ab093bf504515f8c5fe5ed4
qicosmos <qicosmos@linux.alibaba.com>
2024-04-25 15:27:41 +0800

    [coro_http_server]fix content_view (#668)

--- src/coro_http/examples/example.cpp
+++ src/coro_http/examples/example.cpp
@@ -352,6 +352,22 @@ async_simple::coro::Lazy<void> basic_usage() {
         response.set_status_and_content(status_type::ok, "ok");
       });
 
+  server.set_http_handler<POST>(
+      "/view",
+      [](coro_http_request &req,
+         coro_http_response &resp) -> async_simple::coro::Lazy<void> {
+        resp.set_delay(true);
+        resp.set_status_and_content_view(status_type::ok,
+                                         req.get_body());  // no copy
+        co_await resp.get_conn()->reply();
+      });
+  server.set_default_handler(
+      [](coro_http_request &req,
+         coro_http_response &resp) -> async_simple::coro::Lazy<void> {
+        resp.set_status_and_content(status_type::ok, "default response");
+        co_return;
+      });
+
   person_t person{};
   server.set_http_handler<GET>("/person", &person_t::foo, person);
 
@@ -377,6 +393,11 @@ async_simple::coro::Lazy<void> basic_usage() {
   assert(result.status == 200);
   assert(result.resp_body == "post string");
 
+  result = co_await client.async_post("/view", "post string",
+                                      req_content_type::string);
+  assert(result.status == 200);
+  assert(result.resp_body == "post string");
+
   client.add_header("name", "tom");
   client.add_header("age", "20");
   result = co_await client.async_get("/headers");
@@ -388,6 +409,10 @@ async_simple::coro::Lazy<void> basic_usage() {
   result = co_await client.async_get(
       "http://127.0.0.1:9001/users/ultramarines/subscriptions/guilliman");
   assert(result.status == 200);
+
+  result = co_await client.async_get("/not_exist");
+  assert(result.status == 200);
+  assert(result.resp_body == "default response");
 
   // make sure you have install openssl and enable CINATRA_ENABLE_SSL
 #ifdef CINATRA_ENABLE_SSL
