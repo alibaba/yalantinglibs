4f1f86319512268ee26c5c3abca537f778161a2b
qicosmos <qicosmos@linux.alibaba.com>
2024-06-07 16:35:57 +0800

    format

--- src/metric/tests/test_metric.cpp
+++ src/metric/tests/test_metric.cpp
@@ -276,5 +276,5 @@ TEST_CASE("test register metric") {
 }
 
 DOCTEST_MSVC_SUPPRESS_WARNING_WITH_PUSH(4007)
-int main(int argc, char **argv) { return doctest::Context(argc, argv).run(); }
+int main(int argc, char** argv) { return doctest::Context(argc, argv).run(); }
 DOCTEST_MSVC_SUPPRESS_WARNING_POP
