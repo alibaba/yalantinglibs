9ab3d24099bbd80b7c49b3ce30fec250d67a0899
qicosmos <qicosmos@linux.alibaba.com>
2024-06-07 21:06:06 +0800

    data_gen off for test

--- cmake/develop.cmake
+++ cmake/develop.cmake
@@ -26,7 +26,7 @@ if(COVERAGE_TEST)
 endif()
 
 # generator benchmark test data
-option(GENERATE_BENCHMARK_DATA "Generate benchmark data" ON)
+option(GENERATE_BENCHMARK_DATA "Generate benchmark data" OFF)
 message(STATUS "GENERATE_BENCHMARK_DATA: ${GENERATE_BENCHMARK_DATA}")
 
 
