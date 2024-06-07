ea0d790355af6f660c317e3439b23acfb9110065
qicosmos <qicosmos@linux.alibaba.com>
2024-06-07 23:20:01 +0800

    update version to 0.3.3

--- include/ylt/version.hpp
+++ include/ylt/version.hpp
@@ -20,4 +20,4 @@
 // YLT_VERSION % 100 is the sub-minor version
 // YLT_VERSION / 100 % 1000 is the minor version
 // YLT_VERSION / 100000 is the major version
-#define YLT_VERSION 302  // 0.3.2
+#define YLT_VERSION 303  // 0.3.3
