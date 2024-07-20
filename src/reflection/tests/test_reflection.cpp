43d90ce748b268f8eba62a40e3f7d20a7fb363e5
qicosmos <qicosmos@linux.alibaba.com>
2024-07-20 16:16:17 +0800

    rename macro

--- src/reflection/tests/test_reflection.cpp
+++ src/reflection/tests/test_reflection.cpp
@@ -412,17 +412,6 @@ TEST_CASE("test macros") {
   });
 }
 
-
-
-#define REFL_PRIVATE(STRUCT, ...) \
-namespace ylt::reflection {                                 \
-  inline constexpr auto get_private_ptrs(const identity<STRUCT>& t);\
-  template struct private_visitor<STRUCT, __VA_ARGS__>;      \
-}
-
-#define REFL_PRIVATE1(STRUCT, ...) \
-  REFL_PRIVATE(STRUCT, WRAP_ARGS(CONCAT_ADDR, STRUCT, __VA_ARGS__))
-
 class Bank_t{
   int id;
   std::string name;
@@ -430,7 +419,7 @@ class Bank_t{
  public:
   Bank_t(int i, std::string str) : id(i), name(str){}
 };
-REFL_PRIVATE1(Bank_t, id, name);
+REFL_PRIVATE(Bank_t, id, name);
 
 class private_struct{
   int a;
@@ -438,7 +427,7 @@ class private_struct{
  public:
   private_struct(int x, int y) : a(x), b(y) {}
 };
-REFL_PRIVATE1(private_struct, a, b);
+REFL_PRIVATE(private_struct, a, b);
 
 template<typename T, typename Tuple, typename Visitor>
 auto visit_fields_impl(T& t, const Tuple& tp, Visitor&& visitor) {
