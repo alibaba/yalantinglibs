8c5b1e1d6cff6bc5cc2f8b3de62c8848f965bd48
qicosmos <qicosmos@linux.alibaba.com>
2024-07-26 10:06:56 +0800

    for dynamic

--- include/ylt/thread/thread_local.hpp
+++ include/ylt/thread/thread_local.hpp
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <exception>
 #include <memory>
+#include <mutex>
 #include <stdexcept>
 #include <thread>
 #include <unordered_map>
@@ -19,9 +20,7 @@
 #endif
 
 namespace ylt::thread {
-inline thread_local size_t thd_index_;
-
-template <typename T>
+template <typename T, bool is_dynamic = false>
 class thread_local_t {
  public:
   thread_local_t() { create_key(); }
@@ -51,18 +50,42 @@ class thread_local_t {
     auto t = std::make_unique<T>();
     auto ptr = t.get();
     set_tls(ptr);
+    static std::mutex mtx;
+    std::lock_guard lock(mtx);
     thrd_locals_.push_back(std::move(t));
+    if constexpr (is_dynamic) {
+      mtxs_.push_back(std::make_unique<std::mutex>());
+      indexs_.push_back(-1);
+    }
   }
 
-  T &value() {
+  decltype(auto) value() {
     static thread_local size_t index = get_rr();
-    return *thrd_locals_.at(index);
+    if constexpr (is_dynamic) {
+      indexs_[index] = index;
+      return std::make_pair(thrd_locals_.at(index).get(),
+                            mtxs_.at(index).get());
+    }
+    else {
+      return *thrd_locals_.at(index);
+    }
   }
 
   template <typename F>
   void for_each(F &&fn) {
-    for (auto &ptr : thrd_locals_) {
-      fn(*ptr);
+    if constexpr (is_dynamic) {
+      for (size_t i = 0; i < indexs_.size(); i++) {
+        int index = indexs_[i];
+        if (index >= 0) {
+          std::lock_guard lock(*mtxs_[index]);
+          fn(*thrd_locals_[index]);
+        }
+      }
+    }
+    else {
+      for (auto &ptr : thrd_locals_) {
+        fn(*ptr);
+      }
     }
   }
 
@@ -89,10 +112,39 @@ class thread_local_t {
 
   tls_key_t tls_key_;
   std::vector<std::unique_ptr<T>> thrd_locals_;
+  std::vector<std::unique_ptr<std::mutex>> mtxs_;
+  std::vector<int> indexs_;
 };
 
+inline thread_local_t<std::unordered_map<std::string, std::shared_ptr<void>>,
+                      true>
+    g_ylt_tls_map{};
+
+inline decltype(auto) get_tls_pair() { return g_ylt_tls_map.value(); }
+
+inline async_simple::coro::Lazy<void> init_dynamic_thread_locals_impl(
+    size_t thd_num) {
+  std::vector<async_simple::coro::Lazy<async_simple::Try<void>>> vec;
+  for (size_t j = 0; j < thd_num; j++) {
+    auto executor = coro_io::get_global_block_executor();
+    vec.push_back(coro_io::post(
+        [] {
+          g_ylt_tls_map.create_tls();
+        },
+        executor));
+  }
+
+  co_await async_simple::coro::collectAll(std::move(vec));
+}
+
+inline void init_dynamic_thread_locals(
+    size_t thd_num = std::thread::hardware_concurrency()) {
+  coro_io::g_block_io_context_pool(thd_num);
+  async_simple::coro::syncAwait(init_dynamic_thread_locals_impl(thd_num));
+}
+
 template <typename T>
-inline async_simple::coro::Lazy<void> init_thread_locals_impl(
+inline async_simple::coro::Lazy<void> init_static_thread_locals_impl(
     std::vector<thread_local_t<T> *> &tls_list, size_t thd_num) {
   std::vector<async_simple::coro::Lazy<async_simple::Try<void>>> vec;
   for (size_t j = 0; j < thd_num; j++) {
@@ -118,13 +170,14 @@ constexpr inline size_t tls_keys_max() {
 }
 
 template <typename T>
-inline void init_thread_locals(
+inline void init_static_thread_locals(
     std::vector<thread_local_t<T> *> &tls_list,
     size_t thd_num = std::thread::hardware_concurrency()) {
   if (tls_list.size() > tls_keys_max()) {
     throw std::out_of_range("exceed the max number of tls keys");
   }
   coro_io::g_block_io_context_pool(thd_num);
-  async_simple::coro::syncAwait(init_thread_locals_impl<T>(tls_list, thd_num));
+  async_simple::coro::syncAwait(
+      init_static_thread_locals_impl<T>(tls_list, thd_num));
 }
 }  // namespace ylt::thread
