8fec9b19d771a22e4043b257399f5574e32a822d
saipubw <Zezheng.Li@linux.alibaba.com>
2024-08-13 14:57:57 +0800

    [easylog] fix compile error in freeBSD (#749)

--- include/ylt/easylog/record.hpp
+++ include/ylt/easylog/record.hpp
@@ -21,11 +21,15 @@
 
 #include "ylt/util/time_util.h"
 #ifdef __linux__
-#include <sys/syscall.h>
 #include <unistd.h>
 #endif
 #if __has_include(<memory_resource>)
 #include <memory_resource>
+#endif
+#if defined(__linux__) || defined(__FreeBSD__)
+#include <sys/syscall.h>
+#elif defined(__rtems__)
+#include <rtems.h>
 #endif
 #include <sstream>
 #include <string>
