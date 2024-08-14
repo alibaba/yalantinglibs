9b39944b97c96f20bb0d1f3b7e9b81fb52bb1b7e
Zezheng.Li <Zezheng_Li@linux.alibaba.com>
2024-08-14 14:35:57 +0800

    [struct_pack][fix] fix freebsd

--- include/ylt/easylog/record.hpp
+++ include/ylt/easylog/record.hpp
@@ -20,14 +20,12 @@
 #include <cstring>
 
 #include "ylt/util/time_util.h"
-#ifdef __linux__
-#include <unistd.h>
-#endif
 #if __has_include(<memory_resource>)
 #include <memory_resource>
 #endif
 #if defined(__linux__) || defined(__FreeBSD__)
 #include <sys/syscall.h>
+#include <unistd.h>
 #elif defined(__rtems__)
 #include <rtems.h>
 #endif
