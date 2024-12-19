89f23d4392c1aef2f8e0d4188146201ed1e513eb
saipubw <Zezheng_Li@linux.alibaba.com>
2024-12-19 11:22:00 +0800

    add support for uint64 bucket element type for summary. (#858)

--- include/ylt/metric/summary.hpp
+++ include/ylt/metric/summary.hpp
@@ -37,7 +37,7 @@ YLT_REFL(json_summary_t, name, help, type, labels_name, quantiles_key, metrics);
 class summary_t : public static_metric {
  public:
   summary_t(std::string name, std::string help, std::vector<double> quantiles,
-            std::chrono::seconds max_age = std::chrono::seconds{60})
+            std::chrono::seconds max_age = std::chrono::seconds{0})
       : static_metric(MetricType::Summary, std::move(name), std::move(help)),
         quantiles_(std::move(quantiles)),
         impl_(quantiles_,
@@ -133,14 +133,16 @@ class summary_t : public static_metric {
 
  private:
   std::vector<double> quantiles_;
-  ylt::metric::detail::summary_impl<> impl_;
+  ylt::metric::detail::summary_impl<uint64_t> impl_;
 };
 
 template <size_t N>
 class basic_dynamic_summary
-    : public dynamic_metric_impl<ylt::metric::detail::summary_impl<>, N> {
+    : public dynamic_metric_impl<ylt::metric::detail::summary_impl<uint32_t>,
+                                 N> {
  private:
-  using Base = dynamic_metric_impl<ylt::metric::detail::summary_impl<>, N>;
+  using Base =
+      dynamic_metric_impl<ylt::metric::detail::summary_impl<uint32_t>, N>;
 
  public:
   basic_dynamic_summary(
