1fb2d0380d05591ac865a498098ee8852f376b7d
qicosmos <qicosmos@linux.alibaba.com>
2025-02-10 18:20:11 +0800

    test qps

--- src/coro_http/benchmark/main.cpp
+++ src/coro_http/benchmark/main.cpp
@@ -1,72 +1,86 @@
+#include <cstdio>
 #include <ylt/coro_http/coro_http_server.hpp>
 #include <ylt/metric.hpp>
 
 #include "cmdline.h"
+#include "ylt/coro_io/io_context_pool.hpp"
 
 using namespace coro_http;
 
 struct bench_config {
-  int client_num;
   std::string url;
-  int data_size;
-  int server_thd_num;
-  int port;
+  uint32_t client_concurrency;
+  size_t data_len;
+  size_t max_request_count;
+  uint32_t thd_num;
+  unsigned short port;
 };
 
 bench_config init_conf(const cmdline::parser& parser) {
   bench_config conf{};
-  conf.client_num = parser.get<int>("client_num");
   conf.url = parser.get<std::string>("url");
-  conf.data_size = parser.get<int>("data_size");
-  conf.server_thd_num = parser.get<int>("server_thd_num");
-  conf.port = parser.get<int>("port");
+  conf.client_concurrency = parser.get<uint32_t>("client_concurrency");
+  conf.data_len = parser.get<size_t>("data_len");
+  conf.max_request_count = parser.get<size_t>("max_request_count");
+  conf.thd_num = parser.get<uint32_t>("thd_num");
+  conf.port = parser.get<unsigned short>("port");
 
-  CINATRA_LOG_INFO << "client_num: " << conf.client_num << ", "
-                   << "url: " << conf.url << ", "
-                   << "data_size: " << conf.data_size << ", "
-                   << "server_thd_num: " << conf.server_thd_num << ", "
-                   << "port: " << conf.port;
+  std::string method = (conf.data_len == 0) ? "GET" : "POST";
+
+  ELOG_INFO << "url: " << conf.url << ", "
+            << "client concurrency: " << conf.client_concurrency << ", "
+            << "data_len: " << conf.data_len << ", "
+            << "max_request_count: " << conf.max_request_count << ", "
+            << "thd_num: " << conf.thd_num << ", "
+            << "port: " << conf.port << ", method: " << method << ", ";
 
   return conf;
 }
 
 /*
 start clients with get:
-./coro_http_benchmark -c 100 -u http://0.0.0.0:8090/plaintext
+./coro_http_benchmark -c 64 -t 32 -u http://0.0.0.0:8090/plaintext
 start clients with post:
-./coro_http_benchmark -c 100 -u http://0.0.0.0:8090/plaintext -d 12
+./coro_http_benchmark -c 64 -t 32 -u http://0.0.0.0:8090/plaintext -l 12
 only start server:
-./coro_http_benchmark -s 10
+./coro_http_benchmark -t 10
 
 test with local server:
-./coro_http_benchmark -c 100 -u http://0.0.0.0:8090/plaintext -s 10
+./coro_http_benchmark -c 64 -t 32 -u http://0.0.0.0:8090/plaintext
 */
 int main(int argc, char** argv) {
+  easylog::set_min_severity(easylog::Severity::INFO);
   cmdline::parser parser;
-  parser.add<int>("client_num", 'c', "total number of http clients", false, 0);
+
   parser.add<std::string>("url", 'u', "url", false,
                           "http://0.0.0.0:8090/plaintext");
-  parser.add<std::string>("method", 'm', "http method", false, "GET");
-  parser.add<int>("data_size", 'd', "data size", false, 0);
-  parser.add<int>("server_thd_num", 's', "server thread number", false, 0);
-  parser.add<int>("port", 'p', "server port", false, 8090);
+  parser.add<size_t>("data_len", 'l', "data length", false, 0);
+  parser.add<uint32_t>("client_concurrency", 'c',
+                       "total number of http clients", false, 0);
+  parser.add<size_t>("max_request_count", 'm', "max request count", false,
+                     100000);
+  parser.add<uint32_t>("thd_num", 't', "server thread number", false,
+                       std::thread::hardware_concurrency());
+  parser.add<unsigned short>("port", 'p', "server port", false, 8090);
 
   parser.parse_check(argc, argv);
   auto conf = init_conf(parser);
 
-  bool no_server = conf.server_thd_num == 0;
-  bool no_client = conf.client_num == 0;
+  bool no_server = conf.thd_num == 0;
+  bool no_client = conf.client_concurrency == 0;
   if (no_server && no_client) {
-    CINATRA_LOG_INFO << parser.usage();
+    ELOG_INFO << parser.usage();
     return 0;
   }
+
+  coro_io::get_global_executor(conf.thd_num);
 
   std::shared_ptr<coro_http_server> server = nullptr;
 
   if (!no_server) {
     // init server
-    server = std::make_shared<coro_http_server>(
-        conf.server_thd_num, (unsigned short)conf.port, "0.0.0.0");
+    server =
+        std::make_shared<coro_http_server>(conf.thd_num, conf.port, "0.0.0.0");
     server->set_http_handler<GET, POST>(
         "/plaintext", [](request& req, response& resp) {
           resp.set_delay(false);
@@ -83,6 +97,54 @@ int main(int argc, char** argv) {
   }
 
   // benchmark
+  auto pool = coro_io::client_pool<coro_http_client>::create(conf.url);
+
+  bool is_get = conf.data_len == 0;
+  std::string send_data(conf.data_len, 'A');
+  std::string_view url = conf.url;
+  std::string_view data = send_data;
+
+  ylt::metric::counter_t req_count("total_request", "");
+
+  auto lazy = [&]() -> async_simple::coro::Lazy<void> {
+    for (size_t i = 0; i < conf.max_request_count; i++) {
+      co_await pool->send_request(
+          [&](coro_http_client& client) -> async_simple::coro::Lazy<void> {
+            resp_data result;
+            if (is_get) {
+              req_context<> ctx{};
+              result = co_await client.async_request(url, http_method::GET,
+                                                     std::move(ctx));
+            }
+            else {
+              req_context<std::string_view> ctx{};
+              ctx.content = data;
+              result = co_await client.async_request(url, http_method::POST,
+                                                     std::move(ctx));
+            }
+            assert(result.status == 200);
+            assert(result.resp_body == "Hello, world!");
+            req_count.inc();
+          });
+    }
+  };
+
+  for (size_t i = 0; i < conf.client_concurrency; i++) {
+    lazy().start([](auto&&) {
+    });
+  }
+
+  size_t last = 0;
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    auto value = req_count.value();
+    ELOG_INFO << value - last;
+    last = value;
+    if (value == conf.client_concurrency * conf.max_request_count) {
+      ELOG_INFO << "benchmark finished";
+      break;
+    }
+  }
 
   return 0;
 }
