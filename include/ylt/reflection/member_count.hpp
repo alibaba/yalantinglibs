75b2c25eb591e0ee6dd94e3c764f99af761fb6f1
qicosmos <qicosmos@linux.alibaba.com>
2025-03-11 16:17:39 +0800

    [coro_http_server]fix some (#917)

--- include/ylt/reflection/member_count.hpp
+++ include/ylt/reflection/member_count.hpp
@@ -74,10 +74,10 @@ constexpr bool optional = !expected<T> && optional_impl<T>::value;
 
 namespace internal {
 #if __cpp_concepts >= 201907L
-template <typename Type>
-concept tuple_size = requires(Type tuple) {
-  std::tuple_size<remove_cvref_t<Type>>::value;
-};
+  template <typename Type>
+  concept tuple_size = requires(Type tuple) {
+    std::tuple_size<remove_cvref_t<Type>>::value;
+  };
 #else
 template <typename T, typename = void>
 struct tuple_size_impl : std::false_type {};
@@ -91,79 +91,79 @@ template <typename T>
 constexpr bool tuple_size = tuple_size_impl<T>::value;
 #endif
 
-template <typename Type>
-constexpr inline bool is_compatible_v = false;
+  template <typename Type>
+  constexpr inline bool is_compatible_v = false;
 
-template <typename Type, uint64_t version>
-constexpr inline bool is_compatible_v<struct_pack::compatible<Type, version>> =
-    true;
+  template <typename Type, uint64_t version>
+  constexpr inline bool
+      is_compatible_v<struct_pack::compatible<Type, version>> = true;
 
-struct UniversalVectorType {
-  template <typename T>
-  operator std::vector<T>();
-};
+  struct UniversalVectorType {
+    template <typename T>
+    operator std::vector<T>();
+  };
 
-struct UniversalType {
-  template <typename T>
-  operator T();
-};
+  struct UniversalType {
+    template <typename T>
+    operator T();
+  };
 
-struct UniversalIntegralType {
-  template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
-  operator T();
-};
+  struct UniversalIntegralType {
+    template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
+    operator T();
+  };
 
-struct UniversalNullptrType {
-  operator std::nullptr_t();
-};
+  struct UniversalNullptrType {
+    operator std::nullptr_t();
+  };
 
-struct UniversalOptionalType {
-  template <typename U, typename = std::enable_if_t<optional<U>>>
-  operator U();
-};
+  struct UniversalOptionalType {
+    template <typename U, typename = std::enable_if_t<optional<U>>>
+    operator U();
+  };
 
-struct UniversalCompatibleType {
-  template <typename U, typename = std::enable_if_t<is_compatible_v<U>>>
-  operator U();
-};
+  struct UniversalCompatibleType {
+    template <typename U, typename = std::enable_if_t<is_compatible_v<U>>>
+    operator U();
+  };
 
-template <typename T, typename construct_param_t, typename = void,
-          typename... Args>
-struct is_constructable_impl : std::false_type {};
-template <typename T, typename construct_param_t, typename... Args>
-struct is_constructable_impl<
-    T, construct_param_t,
-    std::void_t<decltype(T{{Args{}}..., {construct_param_t{}}})>, Args...>
-    : std::true_type {};
+  template <typename T, typename construct_param_t, typename = void,
+            typename... Args>
+  struct is_constructable_impl : std::false_type {};
+  template <typename T, typename construct_param_t, typename... Args>
+  struct is_constructable_impl<
+      T, construct_param_t,
+      std::void_t<decltype(T{{Args{}}..., {construct_param_t{}}})>, Args...>
+      : std::true_type {};
 
-template <typename T, typename construct_param_t, typename... Args>
-constexpr bool is_constructable =
-    is_constructable_impl<T, construct_param_t, void, Args...>::value;
+  template <typename T, typename construct_param_t, typename... Args>
+  constexpr bool is_constructable =
+      is_constructable_impl<T, construct_param_t, void, Args...>::value;
 
-template <typename T, typename... Args>
-inline constexpr std::size_t members_count_impl() {
-  if constexpr (is_constructable<T, UniversalVectorType, Args...>) {
-    return members_count_impl<T, Args..., UniversalVectorType>();
+  template <typename T, typename... Args>
+  inline constexpr std::size_t members_count_impl() {
+    if constexpr (is_constructable<T, UniversalVectorType, Args...>) {
+      return members_count_impl<T, Args..., UniversalVectorType>();
+    }
+    else if constexpr (is_constructable<T, UniversalType, Args...>) {
+      return members_count_impl<T, Args..., UniversalType>();
+    }
+    else if constexpr (is_constructable<T, UniversalOptionalType, Args...>) {
+      return members_count_impl<T, Args..., UniversalOptionalType>();
+    }
+    else if constexpr (is_constructable<T, UniversalIntegralType, Args...>) {
+      return members_count_impl<T, Args..., UniversalIntegralType>();
+    }
+    else if constexpr (is_constructable<T, UniversalNullptrType, Args...>) {
+      return members_count_impl<T, Args..., UniversalNullptrType>();
+    }
+    else if constexpr (is_constructable<T, UniversalCompatibleType, Args...>) {
+      return members_count_impl<T, Args..., UniversalCompatibleType>();
+    }
+    else {
+      return sizeof...(Args);
+    }
   }
-  else if constexpr (is_constructable<T, UniversalType, Args...>) {
-    return members_count_impl<T, Args..., UniversalType>();
-  }
-  else if constexpr (is_constructable<T, UniversalOptionalType, Args...>) {
-    return members_count_impl<T, Args..., UniversalOptionalType>();
-  }
-  else if constexpr (is_constructable<T, UniversalIntegralType, Args...>) {
-    return members_count_impl<T, Args..., UniversalIntegralType>();
-  }
-  else if constexpr (is_constructable<T, UniversalNullptrType, Args...>) {
-    return members_count_impl<T, Args..., UniversalNullptrType>();
-  }
-  else if constexpr (is_constructable<T, UniversalCompatibleType, Args...>) {
-    return members_count_impl<T, Args..., UniversalCompatibleType>();
-  }
-  else {
-    return sizeof...(Args);
-  }
-}
 }  // namespace internal
 
 template <typename T>
@@ -178,8 +178,11 @@ inline constexpr std::size_t members_count() {
   else if constexpr (internal::tuple_size<type>) {
     return std::tuple_size<type>::value;
   }
-  else {
+  else if constexpr (std::is_aggregate_v<type>) {
     return internal::members_count_impl<type>();
+  }
+  else {
+    static_assert(!sizeof(T), "not supported type!");
   }
 }
 
