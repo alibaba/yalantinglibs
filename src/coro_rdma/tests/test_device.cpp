d40ce0d317268a84ec0bd44b1272dd0a58effc61
qicosmos <qicosmos@linux.alibaba.com>
2025-04-09 15:20:43 +0800

    devices tests

--- src/coro_rdma/tests/test_device.cpp
+++ src/coro_rdma/tests/test_device.cpp
@@ -8,4 +8,10 @@ TEST_CASE("test device list") {
   auto& inst = ib_devices_t::instance();
   auto& names = inst.dev_names();
   CHECK(!names.empty());
+  CHECK(inst.size());
+  auto dev = inst.at(names[0]);
+  CHECK(dev != nullptr);
+
+  CHECK_THROWS_AS(inst[inst.size()], std::out_of_range);
+  CHECK_THROWS_AS(inst.at("no such a device"), std::out_of_range);
 }
