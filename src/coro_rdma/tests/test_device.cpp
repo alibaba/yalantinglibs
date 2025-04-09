fc0427da808ed79bad01f878436fdf83d03d1cbf
qicosmos <qicosmos@linux.alibaba.com>
2025-04-09 16:31:58 +0800

    test device

--- src/coro_rdma/tests/test_device.cpp
+++ src/coro_rdma/tests/test_device.cpp
@@ -12,6 +12,36 @@ TEST_CASE("test device list") {
   auto dev = inst.at(names[0]);
   CHECK(dev != nullptr);
 
+  CHECK(inst.at("") == dev);
+
   CHECK_THROWS_AS(inst[inst.size()], std::out_of_range);
   CHECK_THROWS_AS(inst.at("no such a device"), std::out_of_range);
+}
+
+TEST_CASE("test device") {
+  ib_device_t dev({});
+
+  auto name = dev.name();
+  CHECK(!name.empty());
+  auto ctx = dev.context();
+  CHECK(ctx);
+  auto pd = dev.pd();
+  CHECK(pd);
+  CHECK(dev.port() == 1);
+  CHECK(dev.gid_index() == 1);
+  auto& attr = dev.attr();
+}
+
+TEST_CASE("test device invalid") {
+  CHECK_THROWS_AS(ib_device_t dev({0, "", 3}), std::domain_error);
+  CHECK_THROWS_AS(ib_device_t dev({0, "no such"}), std::out_of_range);
+}
+
+TEST_CASE("test reg_mr") {
+  ib_device_t dev({});
+
+  char buf[256];
+  auto mr = dev.reg_memory(buf, 256);
+  dev.dereg_memory(mr);
+  CHECK(mr);
 }
