714c03f6b9c8f8a3bcf8c43a56fbe1019e7b512c
qicosmos <qicosmos@linux.alibaba.com>
2025-04-09 14:53:34 +0800

    rdma devices

---
+++ include/ylt/coro_rdma/ib_device.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <infiniband/verbs.h>
+
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace ylt::coro_rdma {
+class ib_devices_t {
+ public:
+  static auto& instance() {
+    static ib_devices_t inst{};
+    return inst;
+  }
+  int size() { return size_; }
+  ibv_device*& operator[](int i) {
+    if (i >= size_ || i < 0) {
+      throw std::out_of_range("out of range");
+    }
+    return dev_list_[i];
+  }
+
+  const std::vector<std::string>& dev_names() {
+    return dev_names_;
+  }
+
+ private:
+  ib_devices_t() {
+    dev_list_ = ibv_get_device_list(&size_);
+    if (dev_list_ == nullptr) {
+      size_ = 0;
+    }
+    else {
+      for (int i = 0; i < size_; i++) {
+        dev_names_.push_back(dev_list_[i]->name);
+      }
+      std::sort(dev_names_.begin(), dev_names_.end());
+    }
+  }
+  ~ib_devices_t() {
+    if (dev_list_ != nullptr) {
+      ibv_free_device_list(dev_list_);
+    }
+  }
+
+  int size_;
+  ibv_device** dev_list_;
+  std::vector<std::string> dev_names_;
+};
+}  // namespace ylt::coro_rdma
