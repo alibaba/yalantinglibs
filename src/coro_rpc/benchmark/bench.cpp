30b824f25f4ff399221cc93fc8ea6b4f4992ca28
qicosmos <qicosmos@linux.alibaba.com>
2025-05-16 11:20:05 +0800

    rmda bench

---
+++ src/coro_rpc/benchmark/bench.cpp
@@ -0,0 +1,121 @@
+#include <ylt/coro_io/client_pool.hpp>
+#include <ylt/coro_rpc/coro_rpc_client.hpp>
+#include <ylt/coro_rpc/coro_rpc_server.hpp>
+
+#include "cmdline.h"
+#include "ylt/coro_rpc/impl/protocol/coro_rpc_protocol.hpp"
+
+struct bench_config {
+  std::string url;
+  uint32_t client_concurrency;
+  size_t data_len;
+  size_t max_request_count;
+  unsigned short port;
+  size_t resp_len;
+};
+
+bench_config init_conf(const cmdline::parser& parser) {
+  bench_config conf{};
+  conf.url = parser.get<std::string>("url");
+  conf.client_concurrency = parser.get<uint32_t>("client_concurrency");
+  conf.data_len = parser.get<size_t>("data_len");
+  conf.max_request_count = parser.get<size_t>("max_request_count");
+  conf.port = parser.get<unsigned short>("port");
+  conf.resp_len = parser.get<size_t>("resp_len");
+
+  ELOG_WARN << "url: " << conf.url << ", "
+            << "client concurrency: " << conf.client_concurrency << ", "
+            << "data_len: " << conf.data_len << ", "
+            << "max_request_count: " << conf.max_request_count << ", "
+            << "port: " << conf.port << ", "
+            << "resp_len: " << conf.resp_len;
+
+  return conf;
+}
+
+size_t g_resp_len = 0;
+std::string_view g_resp_str;
+std::atomic<size_t> g_count = 0;
+
+inline std::string_view echo() {
+  auto str = coro_rpc::get_context()->get_request_attachment();
+  if (g_resp_len == 0) {
+    return str;
+  }
+
+  return g_resp_str;
+}
+
+async_simple::coro::Lazy<std::error_code> request(const bench_config& conf) {
+  std::string send_str(conf.data_len, 'A');
+  std::string_view send_str_view(send_str);
+
+  coro_io::client_pool<coro_rpc::coro_rpc_client>::pool_config pool_conf{};
+  pool_conf.client_config.socket_config =
+      coro_rpc::coro_rpc_client::ibverbs_config{};
+  auto pool = coro_io::client_pool<coro_rpc::coro_rpc_client>::create(
+      conf.url, pool_conf);
+  auto lazy = [&]() -> async_simple::coro::Lazy<void> {
+    for (size_t i = 0; i < conf.max_request_count; i++) {
+      co_await pool->send_request([&](coro_rpc::coro_rpc_client& client)
+                                      -> async_simple::coro::Lazy<void> {
+        client.set_req_attachment(send_str_view);
+        auto result = co_await client.call<echo>();
+        if (!result.has_value()) {
+          ELOG_ERROR << result.error();
+          co_return;
+        }
+        g_count += send_str_view.length();
+      });
+    }
+  };
+
+  for (size_t i = 0; i < conf.client_concurrency; i++) {
+    lazy().via(coro_io::get_global_executor()).start([](auto&&) {
+    });
+  }
+
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds{1});
+    auto c = g_count.exchange(0);
+    std::cout << "Throughput:" << 8.0 * c / 1000'000'000 << " Gb/s\n";
+  }
+}
+
+int main(int argc, char** argv) {
+  easylog::set_min_severity(easylog::Severity::WARN);
+  easylog::set_async(true);
+  cmdline::parser parser;
+
+  parser.add<std::string>("url", 'u', "url", false, "0.0.0.0:9000");
+  parser.add<size_t>("data_len", 'l', "data length", false, 7340032);  // 7MB
+  parser.add<uint32_t>("client_concurrency", 'c',
+                       "total number of http clients", false, 0);
+  parser.add<size_t>("max_request_count", 'm', "max request count", false,
+                     100000000);
+
+  parser.add<unsigned short>("port", 'p', "server port", false, 9000);
+  parser.add<size_t>("resp_len", 'r', "response data length", false, 0);
+
+  parser.parse_check(argc, argv);
+  auto conf = init_conf(parser);
+
+  if (conf.client_concurrency == 0) {
+    std::cout << "start server\n";
+    std::string resp_str;
+    if (conf.resp_len != 0) {
+      resp_str = std::string(conf.resp_len, 'A');
+      g_resp_str = resp_str;
+    }
+    coro_rpc::coro_rpc_server server(std::thread::hardware_concurrency(), 9000);
+    server.register_handler<echo>();
+    server.init_ibv();
+    [[maybe_unused]] auto ret = server.start();
+  }
+  else {
+    std::cout << "will start client\n";
+    request(conf).start([](auto&& ec) {
+      ELOG_ERROR << ec.value().message();
+    });
+  }
+}
