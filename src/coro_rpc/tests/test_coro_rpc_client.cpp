1a54b283cf6aa62db54dc94ad49757c461dca41e
Zezheng.Li <lizezheng.lzz@alibaba-inc.com>
2025-07-01 11:21:25 +0800

    fix bazel failed

--- src/coro_rpc/tests/test_coro_rpc_client.cpp
+++ src/coro_rpc/tests/test_coro_rpc_client.cpp
@@ -699,7 +699,7 @@ TEST_CASE("testing client call timeout") {
 #endif
   g_action = {};
 }
-std::string_view echo(std::string_view data);
+static std::string_view echo(std::string_view data) { return data; }
 TEST_CASE("testing client reconnect") {
   coro_rpc_server s1(1, 9002), s2(1, 9003);
   s1.async_start();
