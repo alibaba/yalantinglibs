303ab31a362c875198402ab836da1645fa42e387
qicosmos <qicosmos@linux.alibaba.com>
2025-07-23 16:08:50 +0800

    for rest_rpc

--- src/coro_rpc/examples/base_examples/py_example/py_rpc_example.cpp
+++ src/coro_rpc/examples/base_examples/py_example/py_rpc_example.cpp
@@ -1,11 +1,93 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/pytypes.h>
 
+#include <chrono>
+// #include <rest_rpc.hpp>
 #include <string>
+#include <ylt/coro_rpc/coro_rpc_server.hpp>
 
 namespace py = pybind11;
+
+class py_coro_rpc_server {
+ public:
+  py_coro_rpc_server(size_t thd_num, std::string address, size_t seconds = 0)
+      : server_(thd_num, address, std::chrono::seconds(seconds)) {}
+
+  bool start() {
+    auto ec = server_.start();
+    return ec.val() == 0;
+  }
+
+  bool async_start() {
+    auto ec = server_.async_start();
+    return !ec.hasResult();
+  }
+
+  void stop() { server_.stop(); }
+
+  void register_handler(std::string name, py::function func) {}
+
+ private:
+  coro_rpc::coro_rpc_server server_;
+};
+
+// class py_rest_rpc_server {
+//  public:
+//   py_rest_rpc_server(size_t port, size_t thd_num) : server_(port, thd_num) {}
+
+//   void async_start() { server_.async_run(); }
+
+//   void start() { server_.run(); }
+
+//   void register_handler(std::string name, py::function func) {
+//     server_.register_handler(name,
+//                              [func](rest_rpc::rpc_service::rpc_conn conn,
+//                                     std::string arg) -> std::string {
+//                                py::gil_scoped_acquire acquire;
+//                                auto result = func(arg);
+//                                return result.cast<std::string>();
+//                              });
+//   }
+
+//  private:
+//   rest_rpc::rpc_service::rpc_server server_;
+// };
+
+// class py_rest_rpc_client {
+//  public:
+//   py_rest_rpc_client(std::string host, uint16_t port) : client_(host, port) {}
+
+//   bool connect() { return client_.connect(); }
+
+//   std::string call(std::string func_name, std::string arg) {
+//     return client_.call<std::string>(func_name, arg);
+//   }
+
+//  private:
+//   rest_rpc::rpc_client client_;
+// };
 
 PYBIND11_MODULE(py_coro_rpc, m) {
   m.def("hello", [] {
     return std::string("hello");
   });
+
+  py::class_<py_coro_rpc_server>(m, "coro_rpc_server")
+      .def(py::init<size_t, std::string, size_t>())
+      .def("start", &py_coro_rpc_server::start)
+      .def("async_start", &py_coro_rpc_server::async_start)
+      .def("stop", &py_coro_rpc_server::stop);
+
+  // py::class_<py_rest_rpc_server>(m, "rest_rpc_server")
+  //     .def(py::init<size_t, size_t>())
+  //     .def("start", &py_rest_rpc_server::start)
+  //     .def("async_start", &py_rest_rpc_server::async_start)
+  //     .def("register_handler", &py_rest_rpc_server::register_handler,
+  //          py::arg("name"), py::arg("func"));
+
+  // py::class_<py_rest_rpc_client>(m, "rest_rpc_client")
+  //     .def(py::init<std::string, uint16_t>())
+  //     .def("connect", &py_rest_rpc_client::connect)
+  //     .def("call", &py_rest_rpc_client::call,
+  //          py::call_guard<py::gil_scoped_release>());
 }
--- src/coro_rpc/examples/base_examples/py_example/test_rpc.py
+++ src/coro_rpc/examples/base_examples/py_example/test_rpc.py
@@ -1,3 +1,25 @@
 import py_coro_rpc
 
+def hello(str):
+    return str
+
+def handle_request(data: str) -> str:
+    return f"Processed: {data}"
+
 print(py_coro_rpc.hello())
+# server = py_coro_rpc.coro_rpc_server(2, "0.0.0.0:9004", 10)
+# r = server.async_start()
+# print("start result", r)
+# server.stop()
+rest = py_coro_rpc.rest_rpc_server(9004, 2)
+rest.register_handler("handle_request", handle_request)
+rest.register_handler("hello", hello)
+rest.async_start()
+
+client = py_coro_rpc.rest_rpc_client("127.0.0.1", 9004)
+r = client.connect()
+print("connected: ", r)
+result = client.call("handle_request", "test")
+print(result)
+result = client.call("hello", "world")
+print(result)
