b4a5d77e7bd6ea8f1c5674a105eb263a16da9b20
qicosmos <qicosmos@linux.alibaba.com>
2025-07-31 15:00:15 +0800

    update http py example

--- src/coro_http/examples/py_example/test.py
+++ src/coro_http/examples/py_example/test.py
@@ -7,15 +7,27 @@ async def async_get(url):
     def cpp_callback(message):
         def set_result():
             future.set_result(message)
+            print("set result ok")
 
         loop.call_soon_threadsafe(set_result)
+        print("call_soon_threadsafe")
 
     caller = py_example.caller(cpp_callback)
     caller.async_get(url)
+    print("await")
     result = await future
     print(result)
 
+async def async_get1(url):
+    buf = bytearray(2 * 1024)
+    loop = asyncio.get_running_loop()
+    result, len = await py_example.async_get(loop, url, buf)
+    print(result)
+    print(buf[:len])
+
+
 async def main():
+    await async_get1("http://taobao.com")
     await async_get("http://taobao.com")
 
 if __name__ == "__main__":
