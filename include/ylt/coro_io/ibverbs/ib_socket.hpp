a20b80332cf505ed0d242c63e7ce07f27c3b5e9c
Zezheng.Li <lizezheng.lzz@alibaba-inc.com>
2025-08-05 18:18:46 +0800

    change min buffer cnt to 8

--- include/ylt/coro_io/ibverbs/ib_socket.hpp
+++ include/ylt/coro_io/ibverbs/ib_socket.hpp
@@ -426,8 +426,8 @@ class ib_socket_t {
   // nested struct
   struct config_t {
     uint32_t cq_size = 128;
-    uint16_t recv_buffer_cnt = 4;
-    uint16_t send_buffer_cnt = 4;
+    uint16_t recv_buffer_cnt = 8;
+    uint16_t send_buffer_cnt = 8;
     ibv_qp_type qp_type = IBV_QPT_RC;
     ibv_qp_cap cap = {.max_send_wr = 32,
                       .max_recv_wr = 32,
