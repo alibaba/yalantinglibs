b04d100c65a5a2a49216aa1803a68b5f2ea7b0e8
qicosmos <qicosmos@163.com>
2025-11-21 08:51:49 +0800

    fix

--- include/ylt/coro_rpc/impl/coro_rpc_server.hpp
+++ include/ylt/coro_rpc/impl/coro_rpc_server.hpp
@@ -525,20 +525,8 @@ class coro_rpc_server_base {
     acceptor_close_waiter_.get_future().wait();
   }
 
-  bool is_ip_v6(std::string_view address) {
-    asio::ip::address_v6::bytes_type bytes;
-    unsigned long scope_id = 0;
-
-    struct in6_addr addr;
-    asio::error_code ec;
-    return asio::detail::socket_ops::inet_pton(ASIO_OS_DEF(AF_INET6),
-                                               address.data(), &bytes[0],
-                                               &scope_id, ec) > 0;
-  }
-
   void init_address(std::string address) {
-    if (size_t pos = address.rfind(':');
-        pos != std::string::npos && !is_ip_v6(address)) {
+    if (size_t pos = address.rfind(':'); pos != std::string::npos) {
       auto port_sv = std::string_view(address).substr(pos + 1);
 
       uint16_t port;
